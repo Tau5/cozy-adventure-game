2138bbc493391767bc365780b326b1fcdd3ea418
Tau <tau0@tutanota.com>
2024-03-14 15:55:42 +0100

    Camera: Use invert settings

--- Player/Camera3D.gd
+++ Player/Camera3D.gd
@@ -1,6 +1,7 @@
 extends Camera3D
 @export var player: Node3D
 @export var distance: float = 8
+@onready var settings_manager = get_node("/root/SettingsManager")
 
 var rot_x = 0
 var rot_y = 0
@@ -13,9 +14,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	# TODO: Add sensibility and invert options
-	rot_x += Input.get_axis("camera_left", "camera_right") * ((2 * PI) / 6) * delta
-	rot_y += Input.get_axis("camera_down", "camera_up") * ((2 * PI) / 6) * delta
+	# TODO: Add sensibility
+	var direction_x = -1 if settings_manager.settings.camera_invert_x else 1
+	var direction_y = -1 if settings_manager.settings.camera_invert_y else 1
+
+	rot_x += Input.get_axis("camera_left", "camera_right") * ((2 * PI) / 6) * delta * direction_x
+	rot_y += Input.get_axis("camera_up", "camera_down") * ((2 * PI) / 6) * delta * direction_y
 	
 	# Gamepad and mouse camera movement should be non-blocking so things like gyroscope control with Steam Input 
 	# (which binds gyroscope movement to a simulated mouse) or similar programs can work alongside 
@@ -24,13 +28,13 @@ func _physics_process(delta):
 	# NOTE: Input.get_last_mouse_velocity is relative to pixel movement, contrary to Input.get_axis
 	# maybe we should normalize sensibility based on screen size so if a user changes screen resolution, 
 	# the sensibility won't change
-	rot_x += Input.get_last_mouse_velocity().x / 750 * ((2 * PI) / 6) * delta;
-	rot_y += Input.get_last_mouse_velocity().y / 750 * ((2 * PI) / 6) * delta;
-	
+	rot_x += Input.get_last_mouse_velocity().x / 750 * ((2 * PI) / 6) * delta * direction_x;
+	rot_y += Input.get_last_mouse_velocity().y / 750 * ((2 * PI) / 6) * delta * direction_y;
+
 	# Limit camera movement vertically to not go under player or past above them (It would just invert the camera)
 	if (rot_y > PI/2): rot_y = PI/2;
 	if (rot_y < 0): rot_y = 0;
-	
+
 	# View explanation at https://github.com/Tau5/cozy-adventure-game/issues/2#issuecomment-1980363364
 	# Calculates position of camera based on distance to player so it orbits around de player
 	var pos = Vector3((distance * cos(rot_y)) * cos(rot_x), distance * sin(rot_y), (distance * cos(rot_y)) * sin(rot_x))
